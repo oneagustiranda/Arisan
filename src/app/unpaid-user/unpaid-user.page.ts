8e07eb479cf4ee4c64dcc763eb932ef7471eec92
Lab-05 <andreasenwandi@gmail.com>
2020-03-13 16:44:48 +0700

    add transaction

--- src/app/unpaid-user/unpaid-user.page.ts
+++ src/app/unpaid-user/unpaid-user.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
 import { UserService } from '../services/user.service';
+import { TransactionService } from '../services/transaction.service';
+import { UtilsService } from '../services/utils.service';
 
 @Component({
   selector: 'app-unpaid-user',
@@ -10,15 +12,33 @@ import { UserService } from '../services/user.service';
 export class UnpaidUserPage implements OnInit {
   private groupId = '';
   users: any = [];
+  transaction: any = {};
   myDate: String = new Date().toISOString();
   constructor(
     private activatedRoute: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private transactionService: TransactionService,
+    private router: Router,
+    private utils: UtilsService
   ) { }
 
   ngOnInit() {
   }
 
+  submitTransaction(){
+    this.transaction.username = this.users.username;
+    console.log("transaksi :" + this.transaction);
+    this.transaction.group_id = this.groupId;
+    this.transaction.date = this.myDate;
+    this.transaction.status = 'paid';
+    this.transactionService.createTransaction(this.transaction).subscribe((response) =>{
+      console.log(response);
+      this.utils.showToast('Transaksi Berhasil');
+    }, (err) => {
+      console.log(JSON.stringify(err));
+      this.utils.showToast('Terjadi Kesalahan');
+    });
+  }
   
   getData(){
     this.groupId = this.activatedRoute.snapshot.paramMap.get('id');
@@ -44,6 +64,7 @@ export class UnpaidUserPage implements OnInit {
       console.log(this.users);
       this.getData();
       this.getUsersData();
+      this.submitTransaction();
     })
   }
 }
